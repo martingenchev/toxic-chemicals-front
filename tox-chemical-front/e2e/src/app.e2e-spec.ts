ce0a69e715f3d776a024152bd8dda1b3a6f4bf82
Maite Quiles Cerdà <maite@dhcp-188-39.gjk.dk>
2019-05-21 13:53:43 +0200

    e2e

--- tox-chemical-front/e2e/src/app.e2e-spec.ts
+++ tox-chemical-front/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,6 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
+
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +9,22 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to tox-chemical-front!');
-  });
+  // it('should display welcome message', () => {
+  //   page.navigateTo();
+  //   expect(page.getParagraphText()).toEqual('Welcome to tox-chemical-front!');
+  // });
+
+it('should go to gate', () => {
+page.navigateTo();
+    
+page.getByFormControlName('username').sendKeys('gate');
+// browser.sleep(1000);
+page.getByFormControlName('password').sendKeys('whatever');
+// browser.sleep(1000);
+element(by.id('btnUserlogin')).click();
+// browser.sleep(1000);
+expect(browser.getCurrentUrl()).toEqual(browser.baseUrl+'/gate');
+// browser.sleep(1000);
+});
+
 });
--- tox-chemical-front/e2e/src/app.po.ts
+++ tox-chemical-front/e2e/src/app.po.ts
@@ -8,4 +8,8 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getByFormControlName(name: string) {
+    return element(by.css('[formControlName=' + name + ']'));
+  }
 }
