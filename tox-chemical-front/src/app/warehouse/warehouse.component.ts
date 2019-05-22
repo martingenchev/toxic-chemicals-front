05e9d40dcaae8516fbbe23f8e82f098a304303e2
Martin.G <martin.g.wiredelta@gmail.com>
2019-05-22 22:01:01 +0200

    Service for updating the inventory based on the awaiting tickets

--- tox-chemical-front/src/app/warehouse/warehouse.component.ts
+++ tox-chemical-front/src/app/warehouse/warehouse.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {WarehouseService} from '../services/warehouse.service';
+import {MatTableDataSource, MatSortModule } from '@angular/material';
+import {tickets, ticketDetails} from '../models/ticket-details';
 
 @Component({
   selector: 'app-warehouse',
@@ -7,22 +9,38 @@ import {WarehouseService} from '../services/warehouse.service';
   styleUrls: ['./warehouse.component.css']
 })
 export class WarehouseComponent implements OnInit {
- ActiveIncomeTicket = [];
-  prop: any;
-  displayedColumns: string[] = ['warehouse_id', 'type', 'quantity', 'date', 'actions'];
-  incomeAndOutcome: string;
+
   constructor(private  warehouseServices: WarehouseService) { }
+  ActiveIncomeTicket = [];
+  displayedColumns: string[] = ['warehouse_id', 'type', 'quantity', 'date', 'actions'];
+  incomeAndOutcome: string[] = ['warehouse_id', 'type', 'quantity', 'date', 'actions'];
+  dataSources = new MatTableDataSource(this.ActiveIncomeTicket);
 
   ngOnInit() {
     this.getAllIncomeActiveTickets();
+    console.log('ticket', tickets);
   }
 
   getAllIncomeActiveTickets() {
         this.warehouseServices.getAll().subscribe( data => {
-         this.ActiveIncomeTicket = [] = data;
+          this.ActiveIncomeTicket = [...data];
          console.log(data);
         } , error1 =>  {
           console.log(error1);
         });
       }
+
+  UpdateWarehouse(ticketDetails) {
+    console.log(ticketDetails);
+    this.warehouseServices.AddItemsToInventory(ticketDetails).subscribe(response => {
+      console.log('si', response , ticketDetails);
+      //TODO Add a toast service for feedback
+    }, error => {
+      console.log('error' , error);
+    });
+  }
+  applyFilter(filterValue: string) {
+    this.dataSources.filter = filterValue.trim().toLowerCase();
+    console.log(this.dataSources);
+  }
 }
